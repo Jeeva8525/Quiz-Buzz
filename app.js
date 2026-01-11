4fc9a96a699b4e4cf8354ad0b9c90549620175ee
Jeeva8525 <jeevananthan8507@gmail.com>
2026-01-11 15:27:04 +0530

    bug fix - quiz parameter - search

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import fs from 'fs'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { log } from 'console'; //instead of console.log(), can also use log()
 
 const __filePath = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filePath)
@@ -10,52 +11,52 @@ const app = express();
 
 app.use(express.static('./public'));
 
-let  quiz={};
+let quiz = {};
 
-
-app.get("/api/quiz/:quizID",(req,res)=>{
-    const {quizID} = req.params;
-    if(!quiz[quizID]){
-        return res.status(404).json({success:false,msg:'no such quiz exists!'});
+app.get("/api/quiz/:quizID", (req, res) => {
+    const { quizID } = req.params;
+    if (!quiz[quizID]) {
+        return res.status(404).json({ success: false, msg: 'no such quiz exists!' });
     }
     return res.status(200).json(quiz[quizID]);
 });
 
-app.get("/api/quiz",(req,res)=>{
-    const {search,topic} = req.query;
-    if(!search && !topic){
+app.get("/api/quiz", (req, res) => {
+    // log(req.query)
+    const { search, topic } = req.query;
+    if (!search && !topic) {
         return res.status(200).json(quiz);
     }
-    let filtered_quiz= {};
-    
-    for(let id in quiz){
-        let topicMatch=true;
-        let searchMatch=true;
+    let filtered_quiz = {};
 
-        if(topic){
-            topicMatch=quiz[id].topic.toLowerCase()===topic.toLowerCase();
+    for (let id in quiz) {
+        let topicMatch = true;
+        let searchMatch = true;
+
+        if (topic) {
+            topicMatch = quiz[id].topic.toLowerCase() === topic.toLowerCase();
         }
-        if(search){
-            searchMatch=quiz[id].name.toLowerCase().includes(search.toLowerCase());
+        if (search) {
+            searchMatch = quiz[id].name.toLowerCase() === search.toLowerCase();
         }
 
-        if(topicMatch && searchMatch){
-            filtered_quiz[id]=quiz[id];
+        if (topicMatch && searchMatch) {
+            filtered_quiz[id] = quiz[id];
         }
     }
     return res.status(200).json(filtered_quiz);
 
 });
 
-app.all('/*',(req,res)=>{
-    res.status(404).json({success:'false',msg:'no such page exists'});
+app.all('/*', (req, res) => {
+    res.status(404).json({ success: 'false', msg: 'no such page exists' });
 });
 
-function startServer(){
+function startServer() {
 
-    quiz = JSON.parse(fs.readFileSync(path.resolve(__dirname,'database/quiz.json'),'utf-8'));
-    app.listen(5000,()=>{
-    console.log("server is listening to port 5000");   
+    quiz = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'database/quiz.json'), 'utf-8'));
+    app.listen(5000, () => {
+        console.log("server is listening to port 5000");
     });
 };
 
