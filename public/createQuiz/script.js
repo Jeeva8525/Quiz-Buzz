c5caae8a7632a135db64f06743a71e0ed31a7ad7
Jeeva8525 <jeevananthan8507@gmail.com>
2026-01-14 12:52:08 +0530

    altered template for html and js functions to dynamically generate questions and options

--- public/createQuiz/script.js
+++ public/createQuiz/script.js
@@ -1,35 +1,85 @@
 const quizName = document.getElementById('quizName');
 const quizTopic = document.getElementById('quizTopic');
-const question = document.getElementById('question');
+const questionName = document.getElementById('questionName');
 
 const submitButton = document.getElementById('submitButton');
 
-const optionsContainer = document.getElementById('optionsContainer');
 const addOptionsButton = document.getElementById('addOptionsButton');
 
 let quesNo = 1;
-let optionsNo = 1;
+let optionsNo = [-1,1];
 
-function addOptions(){
-    /* optionsContainer.innerHTML += `
-        <input type="text" id='question-${quesNo}-option-${++optionsNo}'>
-    ` */ 
+function addOptions(qNo) {
+    const correctAnswerQn = document.getElementById('correctAnswerQn'+qNo);
+    // console.log({qNo});
+    const optionsContainer = document.getElementById(`options-${qNo}-container`);
+    optionsContainer.insertAdjacentHTML('beforeend', `
+        <div class="option">
+            <input 
+                type="text" 
+                id='question-${qNo}-option-${++optionsNo[qNo]}'
+                onblur="updateCorrectAnswerChoices(event,${qNo},${optionsNo[qNo]})"
+            >
+            <button onclick="removeOption(event)">X</button>
+        </div>
+    ` )
 
-    //someone check why the above code is not adding more than 2 input boxes ...
-    const inputElement = document.createElement('input');
-    inputElement.type = 'text'
-    inputElement.id = `question-${quesNo}-option-${++optionsNo}`;
-    optionsContainer.append(inputElement)
+    correctAnswerQn.innerHTML += `
+        <option id='answer-${qNo}-option-${optionsNo[qNo]}'>
+        </option>
+    `
 
 }
 
-addOptionsButton.addEventListener('click',()=>{
-    addOptions();
-});
 
-
-// const sendInputValues = () => {
+function updateCorrectAnswerChoices(event, qno, ano) {
+    // console.log('ano',ano);
     
-// }
+    // console.log(`answer-${qno}-option-${ano}`);
+    const selectOption = document.getElementById(`answer-${qno}-option-${ano}`);
+    selectOption.innerHTML = event.target.value;
+}
 
-// submitButton.addEventListener('click',sendInputValues);
+function removeOption(event) {
+    const outerDiv = event.target.parentElement; // get the div by using the x button's event
+    outerDiv.remove();
+    const idNameArray = outerDiv.children[0].id.split('-'); // select the input tag and access its id name
+    // console.log(idNameArray)
+    const selectOption = document.getElementById(`answer-${parseInt(idNameArray[1])}-option-${parseInt(idNameArray[3])}`);
+    selectOption.remove();
+}
+
+
+
+
+function addQuestions() {
+
+    optionsNo.push(0);
+
+    const questionContainer = document.getElementsByClassName('quiz-detail')[2];
+    // console.log(questionContainer);
+    
+    questionContainer.insertAdjacentHTML ('beforeend', `
+        <div class="question-container">
+            <input type="text" id=question-${++quesNo}-name" placeholder="Question Name">
+            <div id="options-${quesNo}-container">
+                <p>Options<button id="addOptionsButton" onclick="addOptions(${quesNo})">Add</button></p>
+                <div class="option">
+                    <input 
+                        type="text" 
+                        id='question-${quesNo}-option-${++optionsNo[quesNo]}'
+                        onblur="updateCorrectAnswerChoices(event,${quesNo},${optionsNo[quesNo]})"
+                    > 
+                    <button>X</button>
+                </div>
+            </div>
+
+            <p>Correct Answer</p>
+            <select name="correctAnswerQn${quesNo}" id="correctAnswerQn${quesNo}">
+                <option id="answer-${quesNo}-option-1"></option>
+            </select>
+        </div>
+    `)
+        
+
+}
