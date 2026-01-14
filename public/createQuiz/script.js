fba1d9ef8c327a35bbb43be63caf8d1cffecd62d
Jeeva8525 <jeevananthan8507@gmail.com>
2026-01-14 14:39:09 +0530

    get the qns array from the user input in the create quiz page

--- public/createQuiz/script.js
+++ public/createQuiz/script.js
@@ -1,16 +1,15 @@
 const quizName = document.getElementById('quizName');
 const quizTopic = document.getElementById('quizTopic');
-const questionName = document.getElementById('questionName');
 
 const submitButton = document.getElementById('submitButton');
 
 const addOptionsButton = document.getElementById('addOptionsButton');
 
 let quesNo = 1;
-let optionsNo = [-1,1];
-
+let optionsNo = [-1, 2]; //optionsNo is 1-based-index  // first question is already created with two default options => optionsNo[1] = 2
+ 
 function addOptions(qNo) {
-    const correctAnswerQn = document.getElementById('correctAnswerQn'+qNo);
+    const correctAnswerQn = document.getElementById('correctAnswerQn' + qNo);
     // console.log({qNo});
     const optionsContainer = document.getElementById(`options-${qNo}-container`);
     optionsContainer.insertAdjacentHTML('beforeend', `
@@ -33,9 +32,6 @@ function addOptions(qNo) {
 
 
 function updateCorrectAnswerChoices(event, qno, ano) {
-    // console.log('ano',ano);
-    
-    // console.log(`answer-${qno}-option-${ano}`);
     const selectOption = document.getElementById(`answer-${qno}-option-${ano}`);
     selectOption.innerHTML = event.target.value;
 }
@@ -57,11 +53,11 @@ function addQuestions() {
     optionsNo.push(0);
 
     const questionContainer = document.getElementsByClassName('quiz-detail')[2];
-    // console.log(questionContainer);
-    
-    questionContainer.insertAdjacentHTML ('beforeend', `
+
+    questionContainer.insertAdjacentHTML('beforeend', `
         <div class="question-container">
-            <input type="text" id=question-${++quesNo}-name" placeholder="Question Name">
+            <p>Question Name <button onclick="removeQuestion(event)">X</button></p>
+            <input type="text" id="question-${++quesNo}-name" placeholder="Question Name">
             <div id="options-${quesNo}-container">
                 <p>Options<button id="addOptionsButton" onclick="addOptions(${quesNo})">Add</button></p>
                 <div class="option">
@@ -70,16 +66,53 @@ function addQuestions() {
                         id='question-${quesNo}-option-${++optionsNo[quesNo]}'
                         onblur="updateCorrectAnswerChoices(event,${quesNo},${optionsNo[quesNo]})"
                     > 
-                    <button>X</button>
+                </div>
+                <div class="option">
+                    <input 
+                        type="text" 
+                        id='question-${quesNo}-option-${++optionsNo[quesNo]}'
+                        onblur="updateCorrectAnswerChoices(event,${quesNo},${optionsNo[quesNo]})"
+                    > 
                 </div>
             </div>
 
             <p>Correct Answer</p>
             <select name="correctAnswerQn${quesNo}" id="correctAnswerQn${quesNo}">
                 <option id="answer-${quesNo}-option-1"></option>
+                <option id="answer-${quesNo}-option-2"></option>
             </select>
         </div>
     `)
-        
 
+
+}
+
+function removeQuestion(event) {
+    const outerDiv = event.target.parentElement.parentElement; // get the div by using the x button's event
+    outerDiv.remove();
+}
+
+function submit() {
+    const qns = [];
+    let questions;
+    for (let x = 1; x <= quesNo; x++) {
+        questions = [];
+        const elt = document.getElementById(`question-${x}-name`);
+        if (!elt)
+            continue;
+
+        questions.push(elt.value);
+
+        const ansElt = document.getElementById(`correctAnswerQn${x}`)
+        const ans = ansElt.options[ansElt.selectedIndex].text;
+        questions.push(ans);
+
+        for (let y = 0; y < ansElt.options.length; y++) {
+            questions.push(ansElt.options[y].text)
+        }
+        qns.push(questions)
+    }
+    console.log(qns);
+
+    const options = [];
 }
